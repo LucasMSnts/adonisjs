d310e9195854a48cc000c47e7172ced81ee09be0
Lucas Mauro <lucas_de_mauro@hotmail.com>
2020-02-06 16:58:46 -0300

    Autenticacao JWT

--- start/routes.js
+++ start/routes.js
@@ -3,5 +3,6 @@
 const Route = use('Route')
 
 Route.post('users', 'UserController.store')
+Route.post('sessions', 'SessionController.store')
 
 // Mostrar lista de rotas no terminal: adonis route:list
