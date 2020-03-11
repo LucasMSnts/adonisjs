2b482383e901a17cbeed8adc522701a532fd68fd
Lucas Mauro <lucas_de_mauro@hotmail.com>
2020-03-11 16:58:46 -0300

    Lidando com CORS

--- config/cors.js
+++ config/cors.js
@@ -16,7 +16,8 @@ module.exports = {
   | Function - Receives the current origin and should return one of the above values.
   |
   */
-  origin: false,
+  // EX.: origin: ['lucas.com.br', 'service.com'],
+  origin: true,
 
   /*
   |--------------------------------------------------------------------------
@@ -29,7 +30,7 @@ module.exports = {
   | Array - An array of allowed methods
   |
   */
-  methods: ['GET', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  methods: ['GET', 'PUT', 'POST', 'DELETE'],
 
   /*
   |--------------------------------------------------------------------------
